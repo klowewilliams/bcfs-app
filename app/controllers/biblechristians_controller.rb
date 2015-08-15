8f516f8c5ab7d0a214c1b09bb6c03d8f24df196d
Kimberly Lowe-Williams <kimberlylowewilliams@gmail.com>
2015-08-15 11:42:18 -0500

    prepping for blog section

--- app/controllers/biblechristians_controller.rb
+++ app/controllers/biblechristians_controller.rb
@@ -2,5 +2,4 @@ class BiblechristiansController < ApplicationController
 
   def index
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: 'biblechristians#index'
 
   get "/biblechristiansfs" => 'biblechristians#index'
+  
 end
